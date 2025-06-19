5e92ac150efc2d55174a074923909cd6e0e00f7a
Oscar695 <oscarmay157@gmail.com>
2025-06-19 14:00:55 +0100

    add_soduku_grid

--- soduku_checker/src/App.jsx
+++ soduku_checker/src/App.jsx
@@ -1,33 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const createRow = (rowIndex) => {
+    const boxes = []
+    const numberInBox = 8
+  for (let i = 0; i < 3; i++) {
+      const box = <div className="numberBox">{numberInBox}</div>
+      boxes.push(box)
+    }
+    return boxes
+  }
+
+const create3By3Grid = () => {
+  const grid = []
+    for (let i = 0; i < 3; i++) {
+      const row = <div>{createRow(i)}</div>
+      grid.push(row)
+    }
+    return grid
+  }
+
+const lineOf3by3Grids = () => {
+  const grid2 = []
+    for (let i = 0; i < 3; i++) {
+      const row2 = <div className="A3By3Grid">{create3By3Grid(i)}</div>
+      grid2.push(row2)
+    }
+    return grid2
+}
+
+const sodukuGrid = () => {
+  const grid3 = []
+  for (let i = 0; i < 3; i++) {
+    const row3 = <div className="sodukuGrid">{lineOf3by3Grids(i)}</div>
+    grid3.push(row3)
+  }
+  return grid3
+}
+
+
+
+
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {sodukuGrid()}
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
