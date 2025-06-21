14065d1902d9d71c548153cdff5ab0d30c2873bd
Oscar695 <oscarmay157@gmail.com>
2025-06-21 16:17:00 +0100

    not_much_idk_what_to_do_to_make_it_work

--- soduku_checker_atempt2/src/App.jsx
+++ soduku_checker_atempt2/src/App.jsx
@@ -3,23 +3,35 @@ import './App.css'
 
 function App() {
   const [currentGuess, setCurrentGuess] = useState("")
-  const [numbers, setNumbers] = useState([])
+  const [numbers, setNumbers] = useState([[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []])
   const [hasWon, setHasWon] = useState(false)
   const [answer, setAnswer] = useState("")
   const [invalidNumber, setInvalidNumber] = useState(false)
   const [loss, setLoss] = useState(false)
   const [counter, setCounter] = useState(0)
+  const [listUpdater, setListUpdater] = useState(true)
+  
+const createList = () => {
+  let list = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+  setListUpdater(false)
+  return list
+}
+
+    //  useEffect(() => {
+    //   for (let i = 0; i < 81; i++) {
+    //     setNumbers([[...numbers], []])
+    //     console.log([[], []])
+    //     console.log(numbers)
+    //     console.log([])
+    //   }
+    // }, []) 
+    // let hello2 = [[hello], [hello]] use to setNumbers to 81 empty arrays
+
+
   const createRow = (rowIndex) => {
-    
-    useEffect(() => {
-      if (counter <= 81) setCounter(counter + 1)
-      if (counter <= 81) setNumbers([...numbers, counter])
-      console.log(numbers)
-      console.log(counter)
-    }, [])
-   
+
     const boxes = []
-      
+  
     for (let i = 0; i < 9; i++) {
       const collemLineThick = <div className="collemLineThick"></div>
       if (i === 0 || i === 3 || i === 6) boxes.push(collemLineThick)
@@ -27,13 +39,29 @@ function App() {
       const collemLineThin = <div className="collemLineThin"></div>
       if (i !== 0 && i !== 3 && i !== 6) boxes.push(collemLineThin)
         
-        const [value, setValue] = useState()
-        
-      const onInputChange = (event) => {
-        let value2 = event.target.value
-        setValue(value2)
-        setNumbers([...numbers, value2])
-        console.log(numbers)
+      const [value, setValue] = useState()
+      const [placeholder, setPlaceholder] = useState()
+
+      const onInputChange = (event) => { 
+      //   if (listUpdater === true) setPlaceholder(createList())
+      //   let list3 = placeholder
+         let value2 = event.target.value
+      //   setValue(value2)
+         let value3 = ((rowIndex * 9) + i)
+      //  // setNumbers(numbers[value3] === ([[9]]))
+      //   let list4 = (list3[value3] = value2)
+      //   console.log(value3)
+      //   console.log(list)
+      //   console.log(list3)
+      //   console.log(list4)
+      let counter = 0
+      {if (counter = 0) {
+        let list = createList()
+        list[value3] = value2 
+        counter = counter + 1
+      }
+          else (list[value3] = value2)
+        }
       }
         const inputBoxClass = (value === undefined) ? "inputBox": "hideInputBox"
         const input = <input type="number" id="guessImput" className={inputBoxClass} onChange={onInputChange}/>
